ca6377e268f1b7eaa968e8b6d658d40f14518fb0
Qadir Jafri <queuejafri@protonmail.com>
2023-03-27 12:09:15 -0400

    hide api key :)

--- public/app.py
+++ public/app.py
@@ -2,7 +2,7 @@ import os
 import openai
 from flask import Flask, render_template, request, jsonify
 
-openai.api_key = "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"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 app = Flask(__name__)
 
 messages = [
