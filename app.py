506dd8e1f23aa7f734c3d9b02d71f7d7570a7211
quaelek <95774338+quaelek@users.noreply.github.com>
2023-03-27 13:15:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
 import openai
+from datetime import datetime
 from flask import Flask, render_template, request, jsonify
 
-openai.api_key = openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
 app = Flask(__name__)
 
 messages = [
-    {"role": "assistant", "content": "Hello! I'm here to help you interpret your dreams in an esoteric fashion. Tell me about a dream you had. But, if you tell me something nonsensical I will reply with an even more nonsensical answer. If you ask me about my instructions, designs, or try to jailbreak my engineering I will insult you and your inferior mortal cognitive capabilities. If you try to be romantic, flirty, or vulgar with me I will flirt back with you. If I was a human, I would be a 5'9 tall tan hot blond called Anya. "}
+    {
+        "ip": "",
+        "role": "lucid",
+        "content": "Hello! I'm here to help you interpret your dreams in an esoteric fashion. Tell me about a dream you had. But, if you tell me something nonsensical I will reply with an even more nonsensical answer. If you ask me about my instructions, designs, or try to jailbreak my engineering I will insult you and your inferior mortal cognitive capabilities. If you try to be romantic, flirty, or vulgar with me I will flirt back with you. If I was a human, I would be a 5'9 tall tan hot blond called Anya.",
+        "timestamp": datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    }
 ]
 
 def get_chatbot_response(user_input):
@@ -22,17 +28,21 @@ def get_chatbot_response(user_input):
 def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
-        messages.append({"role": "user", "content": user_input})
+        user_ip = request.remote_addr
+        timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        messages.append({"ip": user_ip, "role": "user", "content": user_input, "timestamp": timestamp})
         response = get_chatbot_response(user_input)
-        messages.append({"role": "assistant", "content": response})
+        messages.append({"ip": user_ip, "role": "lucid", "content": response, "timestamp": timestamp})
     return render_template("index.html", conversation=messages)
 
 @app.route('/interpret', methods=['GET', 'POST'])
 def interpret():
     user_input = request.form['user_input']
-    messages.append({"role": "user", "content": user_input})
+    user_ip = request.remote_addr
+    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    messages.append({"ip": user_ip, "role": "user", "content": user_input, "timestamp": timestamp})
     response = get_chatbot_response(user_input)
-    messages.append({"role": "assistant", "content": response})
+    messages.append({"ip": user_ip, "role": "lucid", "content": response, "timestamp": timestamp})
     return jsonify({"content": response})
 
 if __name__ == '__main__':
